e0b235c1389bd9ae68cfe7ff37da2867cd8d5c63
Junilton <Junilton.dacosta@hotmail.com>
2025-05-03 15:39:08 -0300

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.menu-toggle');
-  const nav = document.querySelector('.site-nav');
-
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('open');
-  });
-});
